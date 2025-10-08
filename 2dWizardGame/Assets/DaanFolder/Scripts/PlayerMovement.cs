14703fd1a7c70638a8cce587002c61b19cd7a977
DaanWouters <144125515+DaanWouters@users.noreply.github.com>
2025-10-08 09:46:44 +0200

    MoveSpeed

--- 2dWizardGame/Assets/DaanFolder/Scripts/PlayerMovement.cs
+++ 2dWizardGame/Assets/DaanFolder/Scripts/PlayerMovement.cs
@@ -2,6 +2,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+
+    [SerializeField] public Collider2D PlayerCollider;
+    [SerializeField] public Rigidbody2D PlayerRigidbody;
+
+
+
+
+    public float MoveSpeed = 5f;
+    public int health = 100;
+    public int MeleeDamage = 10;
+
+
+
+
+
+
+
+
+
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
